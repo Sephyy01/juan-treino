052e39439c703ad9eab26b2f4501990975ebf261
Juan <juan@beaver.com.br>
2023-01-10 14:04:53 -0300

    atualizações

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+
+
+
+
+function sidebar() {
+    var x = document.getElementById("sidebar");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
